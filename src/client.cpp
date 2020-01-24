87aca44192a17587511a20367fcfcb0ed49b581b
ajay <ajay.gunalan@iit.it>
2020-01-24 18:16:10 +0100

    prob with chrono headers; need to enable c++11 with compiler settings

--- src/client.cpp
+++ src/client.cpp
@@ -5,6 +5,7 @@
 #include "srv_demo_pkg/srvdemo.h"
 #include <iostream>
 #include <sstream>
+#include <chrono>
 
 
 //Defining namespace using in this code
@@ -30,7 +31,7 @@ int main(int argc, char **argv)
 
 	  srv_demo_pkg::srvdemo srv;
 	  std::stringstream ss;
-	  ss << "Sending from Here";
+	  ss << "start";
 	  srv.request.in = ss.str();
 
 
@@ -40,15 +41,37 @@ int main(int argc, char **argv)
 	    ROS_INFO("From Client  [%s], Server says [%s]",srv.request.in.c_str(),srv.response.out.c_str());
 
 	  }
+
 	  else
 	  {
 	    ROS_ERROR("Failed to call service");
 	    return 1;
 	  }
 
-	ros::spinOnce();
+	  std::this_thread::sleep_for(std::chrono::milliseconds(15000));
+
+	  ss.str(std::string());
+	  ss << "stop";
+	  srv.request.in = ss.str();
+
+	  if (client.call(srv))
+	  {
+
+	    ROS_INFO("From Client  [%s], Server says [%s]",srv.request.in.c_str(),srv.response.out.c_str());
+
+	  }
+
+	  else
+	  {
+	    ROS_ERROR("Failed to call service");
+	    return 1;
+	  }
+
+	  std::this_thread::sleep_for(std::chrono::milliseconds(15000));
+
+	  ros::spinOnce();
 	//Setting the loop rate
-	loop_rate.sleep();
+	  loop_rate.sleep();
 
 	}
   return 0;
--- src/server.cpp
+++ src/server.cpp
@@ -13,17 +13,16 @@ bool demo_service_callback(srv_demo_pkg::srvdemo::Request &req,
 	std::stringstream ss;
 	ss << "Received Here";
 	res.out = ss.str();
-	ROS_INFO("From Client [%s], Server says [%s]",req.in.c_str(),res.
-	out.c_str());
+	ROS_INFO("From Client [%s], Server says [%s]",req.in.c_str(),res.out.c_str());
 	return true;
 }
 
 int main(int argc, char **argv)
 {
-ros::init(argc, argv, "demo_service_server");
-ros::NodeHandle n;
-ros::ServiceServer service = n.advertiseService("demo_service", demo_service_callback);
-ROS_INFO("Ready to receive from client.");
-ros::spin();
-return 0;
+	ros::init(argc, argv, "demo_service_server");
+	ros::NodeHandle n;
+	ros::ServiceServer service = n.advertiseService("demo_service", demo_service_callback);
+	ROS_INFO("Ready to receive from client.");
+	ros::spin();
+	return 0;
 }
