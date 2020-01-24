a256bcfa8f677a5323a7c9dabb5c304895b00220
ajay <ajay.gunalan@iit.it>
2020-01-24 17:55:01 +0100

    first commit

---
+++ src/server.cpp
@@ -0,0 +1,29 @@
+
+#include "ros/ros.h"
+#include <iostream>
+#include <sstream>
+
+#include "srv_demo_pkg/srvdemo.h"
+
+using namespace std;
+
+bool demo_service_callback(srv_demo_pkg::srvdemo::Request &req, 
+	srv_demo_pkg::srvdemo::Response &res)
+{
+	std::stringstream ss;
+	ss << "Received Here";
+	res.out = ss.str();
+	ROS_INFO("From Client [%s], Server says [%s]",req.in.c_str(),res.
+	out.c_str());
+	return true;
+}
+
+int main(int argc, char **argv)
+{
+ros::init(argc, argv, "demo_service_server");
+ros::NodeHandle n;
+ros::ServiceServer service = n.advertiseService("demo_service", demo_service_callback);
+ROS_INFO("Ready to receive from client.");
+ros::spin();
+return 0;
+}
