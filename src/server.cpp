5530550761ee84fd76cb628f9bb5e68394960684
ajay <ajay.gunalan@iit.it>
2020-01-27 10:51:17 +0100

    demo service works fine

--- src/server.cpp
+++ src/server.cpp
@@ -10,11 +10,34 @@ using namespace std;
 bool demo_service_callback(srv_demo_pkg::srvdemo::Request &req, 
 	srv_demo_pkg::srvdemo::Response &res)
 {
-	std::stringstream ss;
-	ss << "Received Here";
-	res.out = ss.str();
-	ROS_INFO("From Client [%s], Server says [%s]",req.in.c_str(),res.out.c_str());
-	return true;
+	//std::stringstream ss;
+	//ss << "Received Here";
+	//res.out = ss.str();
+	//ROS_INFO("From Client [%s], Server says [%s]",req.in.c_str(),res.out.c_str());
+	//return true;
+  std::stringstream ss;
+  ss << "Received ";
+  res.out = ss.str();
+  string start="start";
+  string stop="stop";
+
+  ROS_INFO("From Client  [%s], Server says [%s]",req.in.c_str(),res.out.c_str());
+  
+  if(strcmp(req.in.c_str(), start.c_str()) == 0)
+  {
+
+    ROS_INFO("starting the cam \n");
+  }
+  else if(strcmp(req.in.c_str(), stop.c_str()) == 0)
+  {
+
+  	ROS_INFO("closing the cam \n");
+  }
+  else 
+  {
+    ROS_INFO("exception occured \n");
+  }
+  return true;
 }
 
 int main(int argc, char **argv)
